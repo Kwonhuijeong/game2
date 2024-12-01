1d0aea05b2d05fdd546cd5e3286042bc986f38b5
Kwonhuijeong <wjd10901@naver.com>
2024-12-02 01:34:50 +0900

    Update 시험 예상문제.c

--- 시험 예상문제.c
+++ 시험 예상문제.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <time.h>
 #include <windows.h> // Sleep 함수 사용
+#include <conio.h>   // _kbhit()와 _getch() 사용
 
 // 기본 화면 크기 정의
 #define WIDTH 40   // 실제 화면의 가로 크기 (픽셀 단위)
@@ -19,12 +20,11 @@
 // 스크린 버퍼 선언
 char screenBuffer[HEIGHT][WIDTH + 1];
 
-void Elf2DInitScreen()
-{
-    //커서 숨기기
+// 초기화 함수: 커서 숨기기
+void Elf2DInitScreen() {
     CONSOLE_CURSOR_INFO cursorInfo;
     GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
-    cursorInfo.bVisible = FALSE;  // 커서를 숨김
+    cursorInfo.bVisible = FALSE; // 커서를 숨김
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
 }
 
@@ -43,6 +43,9 @@ void setPixel(int x, int y, const char* pixel) {
     if (x >= 0 && x < WIDTH / 2 && y >= 0 && y < HEIGHT) {
         screenBuffer[y][x * 2] = pixel[0];
         screenBuffer[y][x * 2 + 1] = pixel[1];
+    }
+    else {
+        printf("setPixel out of bounds: x=%d, y=%d\n", x, y);
     }
 }
 
@@ -85,6 +88,40 @@ void rotatePoint(int cx, int cy, int* x, int* y, double angle) {
     *y = ry;
 }
 
+// 업데이트 함수
+void update() {
+    // 고정된 A: (0, 0) -> (3, 0)
+    int ax0 = 0, ay0 = 0;
+    int ax1 = 2, ay1 = 0;
+    drawLine(ax0, ay0, ax1, ay1, "AA");
+
+    // B: A의 끝점 (3, 0) -> (3, 3), 반시계 방향 30도 회전
+    int bx0 = ax1, by0 = ay1;
+    int bx1 = 6, by1 = 0;
+    rotatePoint(bx0, by0, &bx1, &by1, DEG_TO_RAD(30));
+    drawLine(bx0, by0, bx1, by1, "BB");
+
+    // C: B의 끝점에서 (3, 3) -> (6, 3), 반시계 방향 30도 회전
+    int cx0 = bx1, cy0 = by1;
+    int cx1 = 12, cy1 = 0;
+    rotatePoint(cx0, cy0, &cx1, &cy1, DEG_TO_RAD(30));
+    drawLine(cx0, cy0, cx1, cy1, "CC");
+
+    // C의 끝점 표시 ('FF')
+    setPixel(cx1, cy1, "FF");
+}
+
+// 렌더링 함수
+void render() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD coord = { 0, 0 };
+    SetConsoleCursorPosition(hConsole, coord);
+
+    for (int y = 0; y < HEIGHT; y++) {
+        printf("%s\n", screenBuffer[y]);
+    }
+}
+
 // 게임 루프
 void gameLoop() {
     int running = 1;
@@ -92,50 +129,36 @@ void gameLoop() {
 
     while (running) {
         clock_t currentFrameTime = clock();
-        lastFrameTime = currentFrameTime;
+        if ((currentFrameTime - lastFrameTime) * 1000 / CLOCKS_PER_SEC >= FRAME_DURATION) {
+            lastFrameTime = currentFrameTime;
 
-        // 화면 초기화
-        initializeBuffer();
+            // 디버그 메시지
+            // printf("Frame update: %ld\n", currentFrameTime);
 
-        // 고정된 A: (0, 0) -> (3, 0)
-        int ax0 = 0, ay0 = 0;
-        int ax1 = 2, ay1 = 0;
-        drawLine(ax0, ay0, ax1, ay1, "AA");
+            // 화면 초기화
+            initializeBuffer();
 
-        // B: A의 끝점 (3, 0) -> (3, 3), 반시계 방향 30도 회전
-        int bx0 = ax1, by0 = ay1;
-        int bx1 = 6, by1 = 0;
-        rotatePoint(bx0, by0, &bx1, &by1, DEG_TO_RAD(30));
-        drawLine(bx0, by0, bx1, by1, "BB");
+            // 업데이트
+            update();
 
-        // C: B의 끝점에서 (3, 3) -> (6, 3), 반시계 방향 30도 회전
-        int cx0 = bx1, cy0 = by1;
-        int cx1 = 12, cy1 = 0;
-        rotatePoint(cx0, cy0, &cx1, &cy1, DEG_TO_RAD(30));
-        drawLine(cx0, cy0, cx1, cy1, "CC");
+            // 렌더링
+            render();
 
-        // C의 끝점 표시 ('FF')
-        setPixel(cx1, cy1, "FF");
-
-        // 스크린 버퍼 출력
-        system("cls");
-        for (int y = 0; y < HEIGHT; y++) {
-            printf("%s\n", screenBuffer[y]);
-        }
-
-        // 사용자 입력 처리
-        if (_kbhit()) {
-            char input = _getch();
-            if (input == 'q') running = 0; // 'q'를 눌러 종료
+            // 사용자 입력 처리
+            if (_kbhit()) {
+                char input = _getch();
+                if (input == 'q') running = 0; // 'q'를 눌러 종료
+            }
         }
 
         // Sleep 호출로 CPU 사용률 절약
-        Sleep(FRAME_DURATION);
+        Sleep(1);
     }
 }
 
 // 메인 함수
 int main() {
+    Elf2DInitScreen(); // 초기화 함수 호출
     initializeBuffer();
     gameLoop(); // 게임 루프 실행
     return 0;
